79b5c09ab901bca67074ee1258db09e2fea362eb
Yosorable <1354464666@qq.com>
2025-02-10 22:19:31 +0800

    publish

---
+++ dist/index.d.mts
@@ -0,0 +1,158 @@
+type HUDType = "success" | "error" | "progress" | "label";
+interface ShowHUDRequest {
+    type?: HUDType;
+    title?: string;
+    subTitle?: string;
+    /**
+     * ms
+     */
+    delay?: number;
+}
+/**
+ * @param title use key if not set
+ * @param key callback arg
+ */
+interface AlertAction {
+    title?: string;
+    style?: "cancel" | "destructive";
+    key: string;
+}
+interface AlertConfig {
+    title?: string;
+    message?: string;
+    preferredStyle?: "alert" | "actionSheet";
+    actions: AlertAction[];
+}
+
+interface MRequestBase {
+    /**
+     * Api name.
+     */
+    api: string;
+}
+interface MRequestWithData<T> extends MRequestBase {
+    data?: T;
+}
+type MRequest<T> = MRequestBase | MRequestWithData<T>;
+
+declare enum MResponseStatusCode {
+    SUCCESS = 0,
+    FAILED = 7
+}
+interface MResponse {
+    /**
+     * Integer, 0 for succeeded, other for failed.
+     * Auto reject in promise when failed.
+     */
+    code: MResponseStatusCode;
+    /**
+     * Error msg or other info.
+     */
+    msg?: string;
+}
+interface MResponseWithData<T> extends MResponse {
+    data: T;
+}
+
+interface AppInfo {
+    name: string;
+    appId: string;
+    author?: string;
+    website?: string;
+    icon?: string;
+    version?: string;
+    description?: string;
+    homepage: string;
+    title?: string;
+    pages?: PageConfig[];
+    tabs?: TabConfig[];
+    navigationBarStatus?: string;
+    colorScheme?: string;
+    backgroundColor?: string;
+    navigationBarColor?: string;
+    tintColor?: string;
+    webServerEnabled?: boolean;
+    landscape?: boolean;
+    files?: File[];
+}
+interface PageConfig {
+    path: string;
+    title?: string;
+    scrollable?: boolean;
+    backgroundColor?: string;
+    navigationBarColor?: string;
+}
+interface TabConfig {
+    path: string;
+    title: string;
+    systemImage: string;
+}
+interface File {
+    name: string;
+    path: string;
+    hash: string;
+}
+
+declare function navigateTo(data: {
+    page: string;
+    title?: string;
+}): Promise<MResponse>;
+declare function navigateBack(delta?: number): Promise<MResponse>;
+declare function redirectTo(data: {
+    page: string;
+    title?: string;
+}): Promise<MResponse>;
+declare function openWebsite(url: string): Promise<MResponse>;
+
+declare function closeApp(): Promise<MResponse>;
+declare function showAppDetail(): Promise<MResponse>;
+declare function installApp(url: string): Promise<MResponse>;
+declare function getInstalledAppList(): Promise<MResponseWithData<AppInfo[]>>;
+
+declare function setNavigationBarTitle(title: string): Promise<MResponse>;
+declare function setNavigationBarColor(config: {
+    foregroundColor: string;
+    backgroundColor: string;
+}): Promise<MResponse>;
+declare function enablePullDownRefresh(): Promise<MResponse>;
+declare function disablePullDownRefresh(): Promise<MResponse>;
+declare function onPullDownRefresh(callback: (e: Event) => any): void;
+declare function startPullDownRefresh(): Promise<MResponse>;
+declare function stopPullDownRefresh(): Promise<MResponse>;
+declare function showHUD(req: ShowHUDRequest): Promise<MResponse>;
+declare function hideHUD(): Promise<MResponse>;
+declare function showAlert(config: AlertConfig): Promise<MResponseWithData<string>>;
+declare function previewImage(url: string): Promise<MResponse>;
+declare function previewVideo(url: string): Promise<MResponse>;
+
+declare function getKVStorage(key: string): Promise<MResponseWithData<string>>;
+declare function setKVStorage(key: string, value: string): Promise<MResponse>;
+declare function deleteKVStorage(key: string): Promise<MResponse>;
+declare function clearKVStorage(): Promise<MResponse>;
+declare function getKVStorageSync(key: string): MResponseWithData<string>;
+declare function setKVStorageSync(key: string, value: string): MResponse;
+declare function deleteKVStorageSync(key: string): MResponse;
+declare function clearKVStorageSync(): MResponse;
+
+/**
+ *
+ * @param type medium as default
+ */
+declare function vibrate(type?: "light" | "medium" | "heavy"): Promise<MResponse>;
+declare function getClipboardData(): Promise<MResponseWithData<string>>;
+declare function setClipboardData(data: string): Promise<MResponseWithData<string>>;
+
+interface Callable {
+    postMessage: (data: string) => Promise<string>;
+}
+declare global {
+    interface Window {
+        webkit?: {
+            messageHandlers?: {
+                MinipNativeInteraction?: Callable;
+            };
+        };
+    }
+}
+
+export { type AlertAction, type AlertConfig, type AppInfo, type HUDType, type MRequest, type MRequestBase, type MRequestWithData, type MResponse, MResponseStatusCode, type MResponseWithData, type ShowHUDRequest, clearKVStorage, clearKVStorageSync, closeApp, deleteKVStorage, deleteKVStorageSync, disablePullDownRefresh, enablePullDownRefresh, getClipboardData, getInstalledAppList, getKVStorage, getKVStorageSync, hideHUD, installApp, navigateBack, navigateTo, onPullDownRefresh, openWebsite, previewImage, previewVideo, redirectTo, setClipboardData, setKVStorage, setKVStorageSync, setNavigationBarColor, setNavigationBarTitle, showAlert, showAppDetail, showHUD, startPullDownRefresh, stopPullDownRefresh, vibrate };
---
+++ dist/index.mjs
@@ -0,0 +1,279 @@
+// src/model/response/index.ts
+var MResponseStatusCode = /* @__PURE__ */ ((MResponseStatusCode2) => {
+  MResponseStatusCode2[MResponseStatusCode2["SUCCESS"] = 0] = "SUCCESS";
+  MResponseStatusCode2[MResponseStatusCode2["FAILED"] = 7] = "FAILED";
+  return MResponseStatusCode2;
+})(MResponseStatusCode || {});
+
+// src/bridge/index.ts
+var jsBridge;
+if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.MinipNativeInteraction) {
+  const _callNative = window.webkit.messageHandlers.MinipNativeInteraction;
+  jsBridge = {
+    callNative(req) {
+      return _callNative.postMessage(JSON.stringify(req)).then((res) => JSON.parse(res)).then((res) => {
+        if (res.code === 0 /* SUCCESS */) {
+          return res;
+        } else {
+          throw new Error(res.msg ?? "Unknown error, res: ");
+        }
+      });
+    },
+    callNativeSync(req) {
+      const res = prompt(JSON.stringify(req));
+      if (res) {
+        return JSON.parse(res);
+      }
+      return {
+        code: 7 /* FAILED */,
+        msg: "Unknown error"
+      };
+    }
+  };
+} else {
+  jsBridge = {
+    callNative() {
+      return new Promise((_, reject) => {
+        reject("Cannot find JavaScript Bridge!!!");
+      });
+    },
+    callNativeSync() {
+      return {
+        code: 7 /* FAILED */,
+        msg: "Cannot find JavaScript Bridge!!!"
+      };
+    }
+  };
+}
+var bridge_default = jsBridge;
+
+// src/api/route.ts
+function navigateTo(data) {
+  return bridge_default.callNative({
+    api: "navigateTo",
+    data
+  });
+}
+function navigateBack(delta = 1) {
+  return bridge_default.callNative({
+    api: "navigateBack",
+    data: {
+      delta
+    }
+  });
+}
+function redirectTo(data) {
+  return bridge_default.callNative({
+    api: "redirectTo",
+    data
+  });
+}
+function openWebsite(url) {
+  return bridge_default.callNative({
+    api: "openWebsite",
+    data: { url }
+  });
+}
+
+// src/api/app.ts
+function closeApp() {
+  return bridge_default.callNative({
+    api: "closeApp"
+  });
+}
+function showAppDetail() {
+  return bridge_default.callNative({
+    api: "showAppDetail"
+  });
+}
+function installApp(url) {
+  return bridge_default.callNative({
+    api: "installApp",
+    data: {
+      url
+    }
+  });
+}
+function getInstalledAppList() {
+  return bridge_default.callNative({
+    api: "getInstalledAppList"
+  });
+}
+
+// src/api/ui.ts
+function setNavigationBarTitle(title) {
+  return bridge_default.callNative({
+    api: "setNavigationBarTitle",
+    data: {
+      title
+    }
+  });
+}
+function setNavigationBarColor(config) {
+  return bridge_default.callNative({
+    api: "setNavigationBarColor",
+    data: config
+  });
+}
+function enablePullDownRefresh() {
+  return bridge_default.callNative({
+    api: "enablePullDownRefresh"
+  });
+}
+function disablePullDownRefresh() {
+  return bridge_default.callNative({
+    api: "disablePullDownRefresh"
+  });
+}
+function onPullDownRefresh(callback) {
+  window.addEventListener("pulldownrefresh", callback);
+}
+function startPullDownRefresh() {
+  return bridge_default.callNative({
+    api: "startPullDownRefresh"
+  });
+}
+function stopPullDownRefresh() {
+  return bridge_default.callNative({
+    api: "stopPullDownRefresh"
+  });
+}
+function showHUD(req) {
+  return bridge_default.callNative({
+    api: "showHUD",
+    data: req
+  });
+}
+function hideHUD() {
+  return bridge_default.callNative({
+    api: "hideHUD"
+  });
+}
+function showAlert(config) {
+  return bridge_default.callNative({
+    api: "showAlert",
+    data: config
+  });
+}
+function previewImage(url) {
+  return bridge_default.callNative({
+    api: "previewImage",
+    data: {
+      url
+    }
+  });
+}
+function previewVideo(url) {
+  return bridge_default.callNative({
+    api: "previewVideo",
+    data: {
+      url
+    }
+  });
+}
+
+// src/api/kvstorage.ts
+function getKVStorage(key) {
+  return bridge_default.callNative({
+    api: "getKVStorage",
+    data: { key }
+  });
+}
+function setKVStorage(key, value) {
+  return bridge_default.callNative({
+    api: "setKVStorage",
+    data: { key, value }
+  });
+}
+function deleteKVStorage(key) {
+  return bridge_default.callNative({
+    api: "deleteKVStorage",
+    data: { key }
+  });
+}
+function clearKVStorage() {
+  return bridge_default.callNative({
+    api: "clearKVStorage"
+  });
+}
+function getKVStorageSync(key) {
+  return bridge_default.callNativeSync({
+    api: "getKVStorageSync",
+    data: { key }
+  });
+}
+function setKVStorageSync(key, value) {
+  return bridge_default.callNativeSync({
+    api: "setKVStorageSync",
+    data: { key, value }
+  });
+}
+function deleteKVStorageSync(key) {
+  return bridge_default.callNativeSync({
+    api: "deleteKVStorageSync",
+    data: { key }
+  });
+}
+function clearKVStorageSync() {
+  return bridge_default.callNativeSync({
+    api: "clearKVStorageSync"
+  });
+}
+
+// src/api/device.ts
+function vibrate(type) {
+  return bridge_default.callNative({
+    api: "vibrate",
+    data: {
+      type
+    }
+  });
+}
+function getClipboardData() {
+  return bridge_default.callNative({
+    api: "getClipboardData"
+  });
+}
+function setClipboardData(data) {
+  return bridge_default.callNative({
+    api: "setClipboardData",
+    data: {
+      data
+    }
+  });
+}
+export {
+  MResponseStatusCode,
+  clearKVStorage,
+  clearKVStorageSync,
+  closeApp,
+  deleteKVStorage,
+  deleteKVStorageSync,
+  disablePullDownRefresh,
+  enablePullDownRefresh,
+  getClipboardData,
+  getInstalledAppList,
+  getKVStorage,
+  getKVStorageSync,
+  hideHUD,
+  installApp,
+  navigateBack,
+  navigateTo,
+  onPullDownRefresh,
+  openWebsite,
+  previewImage,
+  previewVideo,
+  redirectTo,
+  setClipboardData,
+  setKVStorage,
+  setKVStorageSync,
+  setNavigationBarColor,
+  setNavigationBarTitle,
+  showAlert,
+  showAppDetail,
+  showHUD,
+  startPullDownRefresh,
+  stopPullDownRefresh,
+  vibrate
+};
+//# sourceMappingURL=index.mjs.map
